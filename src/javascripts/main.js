1323bcbc0bd984b42fd84fcf79f18f40d74ba164
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-02-24 18:47:21 -0600

    Setup files

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,12 @@
 import '../styles/main.scss';
 
+const printToDom = (divId, textToPrint) => {
+  const selectedDiv = document.querySelector(divId);
+  selectedDiv.innerHTML = textToPrint;
+};
+
 const init = () => {
-  $('#app').html('<h1>HELLO! You are up and running!</h1>');
-  console.log('YOU ARE UP AND RUNNING!');
+  printToDom();
 };
 
 init();
