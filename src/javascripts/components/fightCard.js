465ffa4abcb1a63f472396f337dba52882ff770a
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-02-26 21:11:00 -0600

    Updated card text spacing

--- src/javascripts/components/fightCard.js
+++ src/javascripts/components/fightCard.js
@@ -6,7 +6,7 @@ const fightCard = () => {
   domString += `
     <div class="card" id="eat-card" style="width: auto; height: 100%;">
       <div class="card-body">
-        <h5 class="card-title text-left">FIGHT level = </h5>
+        <h5 class="card-title text-center">FIGHT level = </h5>
         <div class="d-flex flex-column justify-content-center align-items-center">
           <img src="${fight}" class="card-img-top" style="width: 67%" alt="Image of illustrated cat">
           <div id="btns">
--- src/javascripts/components/sleepCard.js
+++ src/javascripts/components/sleepCard.js
@@ -6,7 +6,7 @@ const sleepCard = () => {
   domString += `
     <div class="card" id="eat-card" style="width: auto; height: 100%;">
       <div class="card-body">
-        <h5 class="card-title text-left">ENERGY level = </h5>
+        <h5 class="card-title text-center">ENERGY level = </h5>
         <div class="d-flex flex-column justify-content-center align-items-center">
           <img src="${sleep}" class="card-img-top" style="width: 67%" alt="Image of illustrated cat">
           <div id="btns">
