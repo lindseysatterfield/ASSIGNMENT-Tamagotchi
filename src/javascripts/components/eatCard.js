d1f9b31904d65edb2dae2a60752b953cf3f123e0
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-02-26 20:57:47 -0600

    completed card styling, added modules, styled body

---
+++ src/javascripts/components/eatCard.js
@@ -0,0 +1,23 @@
+import eatCat from '../../assets/eatCat.png';
+import printToDom from '../helpers/printToDom';
+
+const eatCard = () => {
+  let domString = '';
+  domString += `
+    <div class="card" id="eat-card" style="width: auto; height: 100%;">
+      <div class="card-body">
+        <h5 class="card-title text-left">EAT level = </h5>
+        <div class="d-flex flex-column justify-content-center align-items-center">
+          <img src="${eatCat}" class="card-img-top" style="width: 67%" alt="Image of illustrated cat">
+          <div id="btns">
+            <button type="button" class="btn btn-light mx-5">+ food</button>
+            <button type="button" class="btn btn-light mx-5">- food</button>
+          </div>
+        </div>
+      </div>
+    </div>`;
+
+  printToDom('#eat', domString);
+};
+
+export default eatCard;
---
+++ src/javascripts/components/sleepCard.js
@@ -0,0 +1,23 @@
+import sleep from '../../assets/sleep.png';
+import printToDom from '../helpers/printToDom';
+
+const sleepCard = () => {
+  let domString = '';
+  domString += `
+    <div class="card" id="eat-card" style="width: auto; height: 100%;">
+      <div class="card-body">
+        <h5 class="card-title text-left">ENERGY level = </h5>
+        <div class="d-flex flex-column justify-content-center align-items-center">
+          <img src="${sleep}" class="card-img-top" style="width: 67%" alt="Image of illustrated cat">
+          <div id="btns">
+            <button type="button" class="btn btn-light mx-4">+ nap</button>
+            <button type="button" class="btn btn-light mx-4">- deep slumber</button>
+          </div>
+        </div>
+      </div>
+    </div>`;
+
+  printToDom('#sleep', domString);
+};
+
+export default sleepCard;
