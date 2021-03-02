b2dd2f042a8d6d29783a97036728c8f8a36746e6
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-03-02 15:51:21 -0600

    Added STRENGTH functionality

--- src/javascripts/components/cards/fightCard.js
+++ src/javascripts/components/cards/fightCard.js
@@ -6,12 +6,12 @@ const fightCard = () => {
   domString += `
     <div class="card" id="eat-card" style="width: auto; height: 100%;">
       <div class="card-body">
-        <h5 class="card-title text-center">FIGHT level = </h5>
+      <h5 class="card-title text-center" id="fight-level">STRENGTH level = 100</h5>
         <div class="d-flex flex-column justify-content-center align-items-center">
           <img src="${fight}" class="card-img-top" style="width: 67%" alt="Image of illustrated cat">
           <div id="btns">
-            <button type="button" class="btn btn-light mx-4">+ run away</button>
-            <button type="button" class="btn btn-light mx-4">- violence</button>
+            <button type="button" id="add-fight" class="btn btn-light mx-4">+ run away</button>
+            <button type="button" id="subtract-fight" class="btn btn-light mx-4">- violence</button>
           </div>
         </div>
       </div>
--- src/javascripts/helpers/domEvents.js
+++ src/javascripts/helpers/domEvents.js
@@ -1,4 +1,5 @@
 import { subtractFood, addFood } from './data/eatData';
+import { addStrength, subtractStrength } from './data/fightData';
 import { addSuperFun, addRegularFun } from './data/playData';
 
 const domEvents = () => {
@@ -6,6 +7,8 @@ const domEvents = () => {
   document.querySelector('#eat-subtract').addEventListener('click', subtractFood);
   document.querySelector('#play-super-add').addEventListener('click', addSuperFun);
   document.querySelector('#play-regular-add').addEventListener('click', addRegularFun);
+  document.querySelector('#add-fight').addEventListener('click', addStrength);
+  document.querySelector('#subtract-fight').addEventListener('click', subtractStrength);
 };
 
 export default domEvents;
