c0b0da96a8d29f6a1e2d38ce4161011292abddfc
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-03-02 15:37:41 -0600

    Added FUN funcionality

--- src/javascripts/components/cards/eatCard.js
+++ src/javascripts/components/cards/eatCard.js
@@ -6,6 +6,7 @@ const eatCard = () => {
   domString += `
     <div class="card" id="eat-card" style="width: auto; height: 100%;">
       <div class="card-body">
+        <h5 class="card-title text-center" id="eat-level">EAT level = 100</h5>
         <h5 id="max-eat" style="margin: 0;"></h5>
         <div class="d-flex flex-column justify-content-center align-items-center">
           <img src="${eatCat}" class="card-img-top" style="width: 67%" alt="Image of illustrated cat">
--- src/javascripts/helpers/domEvents.js
+++ src/javascripts/helpers/domEvents.js
@@ -1,8 +1,11 @@
 import { subtractFood, addFood } from './data/eatData';
+import { addSuperFun, addRegularFun } from './data/playData';
 
 const domEvents = () => {
   document.querySelector('#eat-add').addEventListener('click', addFood);
   document.querySelector('#eat-subtract').addEventListener('click', subtractFood);
+  document.querySelector('#play-super-add').addEventListener('click', addSuperFun);
+  document.querySelector('#play-regular-add').addEventListener('click', addRegularFun);
 };
 
 export default domEvents;
